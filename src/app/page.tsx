02cd00f96f90f96ac90bad37ded5c201383533ef
iliesben <ilies.benosman@hetic.net>
2023-10-16 10:41:47 +0200

    fix: home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,24 +13,21 @@ export default function Home() {
       <Box mt={100} mb="xl">
         <Container size="md" pb="lg">
           <Title order={1} size={64} ta="center">
-            Simplify your virtual machine configuration effortlessly
+            Simplify your machines configuration effortlessly
           </Title>
         </Container>
         <Container size="sm" pb="lg">
           <Text size="md" ta="center" c="dimmed">
-            Focus on what matters the most, save time and configure your virtual
-            machine in few clicks. You choose, we provide all you need
+            Focus on what matters the most, save time and configure your machine
+          </Text>
+          <Text size="md" ta="center" c="dimmed">
+            in few clicks. You choose we provide
           </Text>
         </Container>
         <Flex justify="center" align="center" direction="column" pb="lg">
           <Box pb={'md'}>
             <Button variant="filled" href="/register">
               Get started
-            </Button>
-          </Box>
-          <Box>
-            <Button variant="arrow" rightSection={<BsArrowRight />}>
-              See community configuration (in progress...)
             </Button>
           </Box>
         </Flex>
