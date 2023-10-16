4fbcc0db3069caf0210322397dd2e034fcd99d53
Ange <ange.goua01@gmail.com>
2023-10-16 11:05:42 +0200

    feat: remove caching

--- src/app/dashboard/getValidConfigs.ts
+++ src/app/dashboard/getValidConfigs.ts
@@ -2,7 +2,6 @@ import { apiServer } from '@/utils/api/apiFactory';
 import { ConfigSchema } from '@/utils/schemas/config.schema';
 import { Config, Configs, ErrorDetails } from '@/utils/types/config.type';
 import { getArrayFirsts } from '@/utils/helpers';
-import { useConfigsStore } from './store/configs';
 
 export const getValidConfigs = async (configCount?: number) => {
   const errorDetails: ErrorDetails = {
@@ -14,27 +13,20 @@ export const getValidConfigs = async (configCount?: number) => {
   };
   const validConfigs: Configs = [];
 
-  const configs = useConfigsStore.getState().configs;
-
-  if (!configs.length) {
-    const res: unknown = await apiServer.get('/configs/me');
-    if (res instanceof Array && res.length > 0) {
-      res.forEach((config: Config) => {
-        const parsedConfig = ConfigSchema.safeParse(config);
-        if (parsedConfig.success) {
-          validConfigs.push(parsedConfig.data);
-        } else {
-          errorDetails.hasSomeUnusableConfigs = true;
-          errorDetails.hiddenConfigs.push({
-            config,
-            error: parsedConfig.error.toString(),
-          });
-        }
-      });
-    }
-    useConfigsStore.setState({ configs: validConfigs });
-  } else {
-    validConfigs.push(...configs);
+  const res: unknown = await apiServer.get('/configs/me');
+  if (res instanceof Array && res.length > 0) {
+    res.forEach((config: Config) => {
+      const parsedConfig = ConfigSchema.safeParse(config);
+      if (parsedConfig.success) {
+        validConfigs.push(parsedConfig.data);
+      } else {
+        errorDetails.hasSomeUnusableConfigs = true;
+        errorDetails.hiddenConfigs.push({
+          config,
+          error: parsedConfig.error.toString(),
+        });
+      }
+    });
   }
 
   const toReverse = (arr: Configs) => [...arr].reverse();
